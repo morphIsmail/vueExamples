5d101a61b8a30824ecacdcc5478721914fd1f8e2
morphIsmail <ismail_2016@mail.ru>
2020-09-17 17:47:44 +0300

    example4 v-for

---
+++ ex4/js/main.js
@@ -0,0 +1,10 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        todos: [
+             { text: 'Изучить JS' },
+             { text: 'Изучить Vue' },
+             { text: 'Создать что-нибудь классное' }
+        ]
+    }
+})
